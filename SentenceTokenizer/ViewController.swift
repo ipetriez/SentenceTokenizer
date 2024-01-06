03aa8df9d8f797217f32553561d984e2d6367a7c
Sergey Petrosyan <ipetriez@icloud.com>
2024-01-06 14:03:02 +0400

    Initial Commit

---
+++ SentenceTokenizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentenceTokenizer
+//
+//  Created by Sergey Petrosyan on 06.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
